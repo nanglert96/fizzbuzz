23b6570a5672ad72b5a662cf07b07b991028fb15
Ryan Nangle <ryannangle@Ryans-MacBook-Pro.local>
2016-03-23 10:53:16 -0400

    fizz,buzz,fizzbuzz tests and code
    q

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -2,8 +2,22 @@
 var fb = require('../fizzbuzz');
 
 describe("a program that impelments the fizzbuzz game", function() {
-	it("count from 1 to 5", function() {
-		expect(fb.count(1, 5)).toBe("1 2 3 4 5");
+	it("count from 1 to 15", function() {
+		expect(fb.count(1, 15)).toBe("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15");
 	});
 
+	it("replaces multiples of 3 with fizz", function() {
+		expect(fb.fizzer(3)).toBe("fizz");
+		expect(fb.fizzer(4)).toBe(4);
+	});
+
+	it("replaces multiples of 5 with buzz", function() {
+		expect(fb.buzzer(5)).toBe("buzz");
+		expect(fb.buzzer(6)).toBe(6);
+	});
+
+	it("replaces multiples of 3 and 5 with fizzbuzz", function() {
+		expect(fb.fizzbuzzer(15)).toBe("fizzbuzz");
+		expect(fb.fizzbuzzer(14)).toBe(14);
+	});
 });
